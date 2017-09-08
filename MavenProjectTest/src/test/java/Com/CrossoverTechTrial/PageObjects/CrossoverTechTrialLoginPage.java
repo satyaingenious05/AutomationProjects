a5ce80795fdffe31e2cd65ef4b580bb4e40f7790
Admin-it <Admin-it@DESKTOP-1QL8QRP>
2017-09-08 12:53:07 +0530

    latest on 8th at 12:54pm

--- MavenProjectTest/src/test/java/Com/CrossoverTechTrial/PageObjects/CrossoverTechTrialLoginPage.java
+++ MavenProjectTest/src/test/java/Com/CrossoverTechTrial/PageObjects/CrossoverTechTrialLoginPage.java
@@ -7,14 +7,14 @@ import org.openqa.selenium.WebElement;
 public class CrossoverTechTrialLoginPage {
 	
 	
-	static String Emailid_xpath = ".//input[@class='email ember-view gh-input']";
+	static String Emailid_xpath = ".//*[@class='email ember-view gh-input']";
 	static String Password_xpath = ".//*[@class ='password ember-view gh-input']";
 	static String SignIn_xpath = ".//*[@class ='login btn btn-blue btn-block ember-view']";
 	
 	
 	public static CrossoverTechTrialLoginPage  EnterEmailid(WebDriver driver) throws InterruptedException{
 		
-		Thread.sleep(8000);
+		Thread.sleep(6000);
 		WebElement Emailid = driver.findElement(By.xpath(Emailid_xpath));
 		if(Emailid.isDisplayed() || Emailid.isEnabled()){
 			Emailid.clear();
